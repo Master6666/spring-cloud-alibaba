cc926c641555da07c6473a5236cfa4ca8de35e84
yangyunfeng666 <445482919@qq.com>
2019-08-04 16:39:08 +0800

    修改配置

--- spring-cloud-alibaba-examples/seata-example/business-service/src/main/java/com/alibaba/cloud/examples/BusinessApplication.java
+++ spring-cloud-alibaba-examples/seata-example/business-service/src/main/java/com/alibaba/cloud/examples/BusinessApplication.java
@@ -18,6 +18,7 @@ package com.alibaba.cloud.examples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
@@ -31,6 +32,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class BusinessApplication {
 
 	public static void main(String[] args) {
--- spring-cloud-alibaba-examples/seata-example/storage-service/src/main/java/com/alibaba/cloud/examples/DatabaseConfiguration.java
+++ spring-cloud-alibaba-examples/seata-example/storage-service/src/main/java/com/alibaba/cloud/examples/DatabaseConfiguration.java
@@ -85,9 +85,9 @@ public class DatabaseConfiguration {
 
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSourceProxy);
 
-		jdbcTemplate.update("delete from storage_tbl where commodity_code = 'C00321'");
-		jdbcTemplate.update(
-				"insert into storage_tbl(commodity_code, count) values ('C00321', 100)");
+//		jdbcTemplate.update("delete from storage_tbl where commodity_code = 'C00321'");
+//		jdbcTemplate.update(
+//				"insert into storage_tbl(commodity_code, count) values ('C00321', 100)");
 
 		return jdbcTemplate;
 
